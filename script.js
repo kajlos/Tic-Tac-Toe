a16594293d7ef893f51b8b9676a4cc8b349e6d29
Kajetan <kajetan.loska@op.pl>
2022-02-27 18:03:49 +0100

    fsd

--- script.js
+++ script.js
@@ -14,29 +14,34 @@ const game =(()=>{
             currentPlayer = player1;
         }
     }
+    const checkwin =(e)=>{
+        let currentGrid = Array.from(document.querySelectorAll('[data-index]'));
+        console.log(currentGrid);
+        console.log(e);
+    }
     const click= e=>{
         if(e.target.textContent == ''){
+            checkwin(e);
             e.target.innerText = currentPlayer.marker;
             switchPlayer(currentPlayer);
-        }else{
-
         }
-        
     }
     const displayGrid = () => { 
         for(let i=0;i < grid.length; i++){
             let cell= document.createElement('div');
             cell.classList.add('game-cell');
+            cell.dataset.index=i;
             cell.textContent = grid[i];
             cell.onclick = click;
             gameGrid.append(cell);
         }
     }
-    console.log(player1);
-    return{
-        displayGrid,
-    };
+    const resetGrid = ()=>{
+        while(gameGrid.firstChild){
+            gameGrid.removeChild(gameGrid.firstChild);
+        }
+    }
+    return{ displayGrid, resetGrid};
 })();
 
 game.displayGrid();
-console.log(typeof(game));
