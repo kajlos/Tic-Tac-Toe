2eaf7dab7251fcc7c5550815846685d5d7ae69d2
Kajetan <kajetan.loska@op.pl>
2022-02-28 14:19:52 +0100

    done

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const gameGrid = document.getElementById('gameGrid');
-const Player =(marker)=>{
-    return{marker};
+const result = document.getElementById('results');
+const resetButton = document.getElementById('reset')
+const Player =(marker, name)=>{
+    return{marker, name};
 }
 const game =(()=>{
     let grid =['','','','','','','','',''];
-    const player1 = Player('x');
-    const player2 = Player('o');
+    const player1 = Player('x','Player1');
+    const player2 = Player('o','Player2');
     let currentPlayer = player1;
+    let isGameOver = false;
+    const winingCombinations = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
     const switchPlayer = (player)=>{
         if(player ==player1){
             currentPlayer = player2;
@@ -14,17 +27,40 @@ const game =(()=>{
             currentPlayer = player1;
         }
     }
-    const checkwin =(e)=>{
+    const checkwin =()=>{
         let currentGrid = Array.from(document.querySelectorAll('[data-index]'));
-        console.log(currentGrid);
-        console.log(e);
+        return winingCombinations.some(item=>{
+            return item.every(index=>{
+                return currentGrid[index].innerText == currentPlayer.marker;
+            })
+        })
     }
     const click= e=>{
-        if(e.target.textContent == ''){
-            checkwin(e);
-            e.target.innerText = currentPlayer.marker;
-            switchPlayer(currentPlayer);
+        if(!isGameOver){
+            if(e.target.textContent == ''){
+                e.target.innerText = currentPlayer.marker; 
+                if(checkwin()){
+                    result.innerText=currentPlayer.name +' won';
+                    isGameOver=true;
+                };
+                console.log(checkDraw());
+                if(checkDraw()){
+                    result.innerText="Draw";
+                    isGameOver=true;
+                }
+                switchPlayer(currentPlayer);
+            }   
+        }else{
+
         }
+       
+    }
+    const checkDraw =()=>{
+        let currentGrid = Array.from(document.querySelectorAll('[data-index]'));
+        console.log(currentGrid);
+        return currentGrid.every(item=>{
+            return item.textContent == 'x' || item.textContent == 'o';
+        })
     }
     const displayGrid = () => { 
         for(let i=0;i < grid.length; i++){
@@ -37,10 +73,17 @@ const game =(()=>{
         }
     }
     const resetGrid = ()=>{
+        
         while(gameGrid.firstChild){
             gameGrid.removeChild(gameGrid.firstChild);
         }
     }
+    resetButton.addEventListener('click',()=>{
+        isGameOver=false;
+        resetGrid();
+        displayGrid();
+        result.textContent="\u00A0"
+    });
     return{ displayGrid, resetGrid};
 })();
 
